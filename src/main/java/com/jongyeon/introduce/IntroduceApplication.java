aafe430e2b54475cde0be6cf77e985ab37febeb1
whddus9509 <whddus9509@naver.com>
2020-08-04 20:44:54 +0900

    new project

---
+++ src/main/java/com/jongyeon/introduce/IntroduceApplication.java
@@ -0,0 +1,13 @@
+package com.jongyeon.introduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroduceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroduceApplication.class, args);
+    }
+
+}
