aef157ac7325ccd83ac043b2e81698affeda6276
whddus9509 <whddus9509@naver.com>
2020-08-05 11:43:59 +0900

    Update view

--- src/main/java/com/jongyeon/introduce/controller/homeController.java
+++ src/main/java/com/jongyeon/introduce/controller/homeController.java
@@ -1,13 +1,25 @@
 package com.jongyeon.introduce.controller;
 
+import com.jongyeon.introduce.entity.BasicInformation;
+import com.jongyeon.introduce.service.PortfolioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class homeController {
 
+    @Autowired
+    private PortfolioService portfolioService;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        BasicInformation b=portfolioService.returnBasicInformation();
+        model.addAttribute("basic",b);
+
         return "home";
 
     }
