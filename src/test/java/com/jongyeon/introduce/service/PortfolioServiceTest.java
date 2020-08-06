87c7e8d0b0d00a6b980b0ab1b2dff3caf3c54e83
whddus9509 <whddus9509@naver.com>
2020-08-06 16:40:26 +0900

    Update PortfolioServiceTest.java

--- src/test/java/com/jongyeon/introduce/service/PortfolioServiceTest.java
+++ src/test/java/com/jongyeon/introduce/service/PortfolioServiceTest.java
@@ -1,5 +1,7 @@
 package com.jongyeon.introduce.service;
 
+import com.jongyeon.introduce.dto.CertificateDto;
+import com.jongyeon.introduce.entity.Certificate;
 import com.jongyeon.introduce.entity.Education;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -24,4 +27,23 @@ public class PortfolioServiceTest {
             System.out.println(e.getName());
         }
     }
+    @Test
+    public void updateBasicInfoTest(){
+        portfolioService.updateBasicInformation(1L);
+    }
+
+    @Test
+    public void insertCertificateTest(){
+        CertificateDto certificateDto=new CertificateDto()
+                .builder()
+                .name("Opic")
+                .className("IM2")
+                .issuingAgency("Opic")
+                .takeTime(LocalDate.of(2020,1,22))
+                .category("어학")
+                .build();
+        portfolioService.insertCertificate(certificateDto);
+
+    }
+    
 }
