eccf60e47fa05b01e91129996aa9cba4364ee3e0
whddus9509 <whddus9509@naver.com>
2020-08-06 16:40:21 +0900

    Create CertificateRepository.java

---
+++ src/main/java/com/jongyeon/introduce/repository/CertificateRepository.java
@@ -0,0 +1,12 @@
+package com.jongyeon.introduce.repository;
+
+import com.jongyeon.introduce.entity.Certificate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CertificateRepository extends JpaRepository<Certificate,Long> {
+    List<Certificate> findAllByCategoryIs(String s);
+}
